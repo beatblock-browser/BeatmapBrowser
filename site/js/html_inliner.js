9099ec70639158f641583c6ae43e2b378795e559
BigBadE <bigbade333@gmail.com>
2024-11-04 00:06:31 -0600

    Fix inlining race condition

--- site/js/html_inliner.js
+++ site/js/html_inliner.js
@@ -3,10 +3,13 @@
  * and the footer id with the footer.
  * Prevents me having to write the same code on a bunch of pages.
  */
+const event = new Event("FinishInline");
 $(document).ready(function () {
     load_if_real('#navbar', 'navbar.html');
     load_if_real('#searchbar', 'searchbar.html');
-    load_if_real('#search-result-template', 'search_result.html');
+    load_if_real('#search-result-template', 'search_result.html', function () {
+        dispatchEvent(event)
+    });
     load_if_real('#search-result-template-long', 'search_result.html', function () {
         $(this).children()[0].classList.remove('col-md-6');
     });
