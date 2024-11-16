12298af47cebb29ade8db3cf88b7a80d1f67e348
BigBadE <bigbade333@gmail.com>
2024-11-16 17:28:31 -0600

    Added more logging

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,21 +1,26 @@
 . config/config.config
 
-# Secure certs
+echo "Writing certs"
 echo $SITE_CERT >> /etc/ssl/certs/site.crt
-chown root:root /etc/ssl/certs/site.crt
+sudo chown root:root /etc/ssl/certs/site.crt
 echo $SITE_KEY >> /etc/ssl/private/site.key
-chown root:root /etc/ssl/private/site.key
+sudo chown root:root /etc/ssl/private/site.key
 chmod 600 /etc/ssl/private/site.key
 
-# Setup site
+echo "Setting up nginx site"
 cp config/sites-available/*.conf /etc/nginx/sites-available/
 sed -i -e "s/{DOMAIN}/$DOMAIN/g" /etc/nginx/sites-available/*.conf
 
-# Setup nginx
+echo "Enabling site"
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/www.site /etc/nginx/sites-enabled/
 
-surrealdb start --bin 127.0.0.1:8000 &
-nginx &
+echo "Starting nginx"
+sudo systemctl reload nginx
+sudo ufw allow 'Nginx Full'
 
+echo "Starting surrealdb"
+surrealdb start --bin 127.0.0.1:8000 &
+
+echo "Starting the website"
 exec /usr/local/bin/backend
