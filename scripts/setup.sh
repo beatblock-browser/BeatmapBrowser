8d968e3063f0d745a88514f753cbdc77d14f85cb
BigBadE <bigbade333@gmail.com>
2024-11-18 13:54:29 -0600

    Added new prints

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+echo -e "Setting up"
 if [ -f /usr/local/config/config.config ]; then
   . /usr/local/config/config.config
 else
   . /usr/local/config/example.config
 
 # Secure certs
+echo -e "Writing certs"
 echo $SITE_CERT >> /etc/ssl/certs/site.crt
 chown root:root /etc/ssl/certs/site.crt
 echo $SITE_KEY >> /etc/ssl/private/site.key
@@ -12,17 +14,16 @@ chown root:root /etc/ssl/private/site.key
 chmod 600 /etc/ssl/private/site.key
 
 # Setup site
+echo -e "Setting up site"
 cp /usr/local/config/sites-available/*.conf /etc/nginx/sites-available/
 sed -i -e "s/{DOMAIN}/$DOMAIN/g" /etc/nginx/sites-available/*.conf
-
-# Setup nginx
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/www.site.conf /etc/nginx/sites-enabled/www.site.conf
 
 # Setup surrealdb
 curl -sSf https://install.surrealdb.com | sh
 
-echo "Running surrealdb"
+echo -e "Running surrealdb"
 surreal start --user root --pass root "rocksdb:/usr/local/db" &
 sleep 5
 
